1f8472a93f3462aa17b3acc81256961d5fc25e3e
Aan Ardiyana <aanardiyana2@gmail.com>
2017-11-22 18:17:50 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* ======================
+    MAGNIFIC POP UP INIT
+    ========================= */
+    $('.video-popup').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+                }
+            }
+        },
+        zoom: {
+            enabled: true,
+        }
+    });
